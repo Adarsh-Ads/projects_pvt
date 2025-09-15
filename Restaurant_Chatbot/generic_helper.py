b1e74a9cc9e8c94d092efe779ed0bc75c23f3303
Adarsh <adarshjena.ctc@gmail.com>
2025-09-15 08:04:52 +0530

    Update ClassificationLogs submodule reference

---
+++ Restaurant_Chatbot/generic_helper.py
@@ -0,0 +1,7 @@
+import re
+def extract_session_id(session_str):
+    match=re.search(r"/sessions/(.*?)/contexts/",session_str)
+    return match.group(1)
+
+def get_str_from_food_dict(food_dict:dict):
+    return ", ".join ([f"{int(value)} {key}" for key,value in food_dict.items()])
